1c2feb5068341339a7b185378b13bf624efed222
leoVolk <leovolkmann@googlemail.com>
2024-07-14 19:54:22 +0200

    feat: :sparkles: fixed profile page

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -20,161 +20,156 @@ class _ProfileViewState extends State<ProfileView>
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       extendBodyBehindAppBar: true,
-/*         appBar: AppBar(
-          forceMaterialTransparency: true,
-          leading: Padding(
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+        leading: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: CircleAvatar(
+              child: IconButton(
+            icon: Icon(
+              Icons.chevron_left,
+              size: 24,
+            ),
+            onPressed: () => Navigator.pop(context),
+          )),
+        ),
+        actions: [
+          Padding(
             padding: EdgeInsets.symmetric(horizontal: 8.0),
             child: CircleAvatar(
                 child: IconButton(
               icon: Icon(
-                Icons.chevron_left,
+                Icons.add,
                 size: 24,
               ),
               onPressed: () => Navigator.pop(context),
             )),
           ),
-          actions: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              child: CircleAvatar(
-                  child: IconButton(
-                icon: Icon(
-                  Icons.add,
-                  size: 24,
-                ),
-                onPressed: () => Navigator.pop(context),
-              )),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              child: CircleAvatar(
-                  child: IconButton(
-                icon: Icon(
-                  Icons.add,
-                  size: 24,
-                ),
-                onPressed: () => Navigator.pop(context),
-              )),
-            )
-          ],
-        ), */
-/*       body: Column(
-        children: [
-          SizedBox(
-            height: 356,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              clipBehavior: Clip.hardEdge,
-              child: Stack(
-                children: [
-                  Image.network(
-                    'https://pbs.twimg.com/profile_banners/605734189/1708339393/1500x500',
-                    height: double.infinity,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                  SafeArea(
-                      bottom: false,
-                      child: Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                    child: CircleAvatar(
-                                  radius: 64,
-                                  child: Text('PR'),
-                                ))
-                              ],
-                            )
-                          ],
-                        ),
-                      ))
-                ],
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: CircleAvatar(
+                child: IconButton(
+              icon: Icon(
+                Icons.add,
+                size: 24,
               ),
-            ),
-          ),
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: Text(
-                  'User Name',
-                  style: TextStyle(
-                    fontSize: 32,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 48,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              physics: ClampingScrollPhysics(),
-              shrinkWrap: true,
-              children: List.generate(
-                  10,
-                  (index) => Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 4),
-                        child: FilledButton(
-                          style: ButtonStyle(
-                              padding: WidgetStatePropertyAll(
-                                EdgeInsets.symmetric(horizontal: 12),
-                              ),
-                              backgroundColor: WidgetStatePropertyAll(
-                                  Theme.of(context)
-                                      .colorScheme
-                                      .secondaryContainer)),
-                          onPressed: () => {},
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.flag,
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .onSecondaryContainer,
-                              ),
-                              Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 4)),
-                              Text(
-                                'Badge Name',
-                                style: TextStyle(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onSecondaryContainer),
-                              )
-                            ],
-                          ),
-                        ),
-                      )),
-            ),
-          ),
+              onPressed: () => Navigator.pop(context),
+            )),
+          )
         ],
-      ), */
+      ),
       body: NestedScrollView(
           headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
             return <Widget>[
-              SliverAppBar(
-                automaticallyImplyLeading: false,
-                pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  title: TabBar(
-                    controller: _tabController,
-                    tabs: [
-                      Tab(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [Icon(Icons.list), Text('Overview')],
+              SliverToBoxAdapter(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 356,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        clipBehavior: Clip.hardEdge,
+                        child: Stack(
+                          children: [
+                            Image.network(
+                              'https://pbs.twimg.com/profile_banners/605734189/1708339393/1500x500',
+                              height: double.infinity,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            ),
+                            SafeArea(
+                                bottom: false,
+                                child: Padding(
+                                  padding: EdgeInsets.all(8),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Expanded(
+                                              child: CircleAvatar(
+                                            radius: 64,
+                                            child: Text('PR'),
+                                          ))
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ))
+                          ],
                         ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Text(
+                            'User Name',
+                            style: TextStyle(
+                              fontSize: 32,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 48,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        physics: ClampingScrollPhysics(),
+                        shrinkWrap: true,
+                        children: List.generate(
+                            10,
+                            (index) => Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(horizontal: 4),
+                                  child: FilledButton(
+                                    style: ButtonStyle(
+                                        padding: WidgetStatePropertyAll(
+                                          EdgeInsets.symmetric(horizontal: 12),
+                                        ),
+                                        backgroundColor: WidgetStatePropertyAll(
+                                            Theme.of(context)
+                                                .colorScheme
+                                                .secondaryContainer)),
+                                    onPressed: () => {},
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.flag,
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .onSecondaryContainer,
+                                        ),
+                                        Padding(
+                                            padding: EdgeInsets.symmetric(
+                                                horizontal: 4)),
+                                        Text(
+                                          'Badge Name',
+                                          style: TextStyle(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .onSecondaryContainer),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                )),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+                    TabBar(controller: _tabController, tabs: [
+                      Tab(
+                        text: 'Overview',
                       ),
                       Tab(
                         text: 'History',
                       )
-                    ],
-                  ),
+                    ])
+                  ],
                 ),
               )
             ];
@@ -183,17 +178,19 @@ class _ProfileViewState extends State<ProfileView>
             controller: _tabController,
             children: [
               ListView(
+                padding: EdgeInsets.zero,
                 children: List.generate(
                     200,
                     (index) => Card(
-                          child: Text('FOOBAR$index'),
+                          child: Text('Test$index'),
                         )),
               ),
               ListView(
+                padding: EdgeInsets.zero,
                 children: List.generate(
                     200,
                     (index) => Card(
-                          child: Text('FOOBAR$index'),
+                          child: Text('Test$index'),
                         )),
               )
             ],
